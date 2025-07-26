f5f7ff688680c68246cbe73c431e5d0c120a3deb
EsraaKamel11 <Israa.kamel811@gmail.com>
2025-07-26 17:13:17 +0300

    Delete main_2.py

--- main_2.py
+++
@@ -1,2 +0,0 @@
-from ml_pipeline.config.settings import settings, logger
-from ml_pipeline.src.data_processing.cleaner import DataCleaner, QualityFilter, Normalizer, StorageManager, MetadataHandler, Deduplicator, QAGenerator, QAGenerationConfig
